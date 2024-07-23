ad33ae2042103326f30366a1d6b9b1ebcda0f12f
Eber Bonilla <bhuuuje@gmail.com>
2024-07-23 11:25:13 -0600

    nuevos cambios

--- src/js/glider.js
+++ src/js/glider.js
@@ -1,11 +1,23 @@
 new Glider(document.querySelector('.glider'), {
-    slidesToShow: 5,
-    slidesToShow: 5.5,
+    slidesToShow: 5.2,
+    slidesToScroll: 5,
     draggable: true,
     dots: '.dots',
     
     arrows: {
       prev: '.glider-prev',
       next: '.glider-next'
+    }
+  });
+
+  new Glider(document.querySelector('.gliderm'), {
+    slidesToShow: 5.2,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+
+    arrows: {
+      prev: '.glider-prevv',
+      next: '.glider-nextt'
     }
   });
